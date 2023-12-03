8e1791925b4d0336b4c31b683275ce004fc491fd
Sara Usevska <sarausevska42@yahoo.com>
2023-12-03 20:45:34 +0100

    first commit

---
+++ Menuvacnica.java
@@ -0,0 +1,12 @@
+package project_1;
+
+public class Menuvacnica {
+
+	public static void main(String[] args) {
+		int a=70;
+		double x = a*61.6;
+		System.out.println("70 evra vo denari se:");
+		System.out.println(x);
+	}
+
+}
